be112f02087546d174fba378be9135407fc1c2ec
mkyper <mrckyper@gmail.com>
2021-08-01 17:09:35 +0200

    Initial Framework

--- client/src/com/mkypr/gl2/client/SettingsPane.java
+++ client/src/com/mkypr/gl2/client/SettingsPane.java
@@ -1,6 +1,14 @@
 package com.mkypr.gl2.client;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class SettingsPane extends JPanel {
+    public SettingsPane() {
+        setFocusable(false);
+        setLayout(null);
+        setBorder(BorderFactory.createLineBorder(Color.black, 15));
+        setBackground(Color.white);
+
+    }
 }
